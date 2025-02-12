b78a598a65cf2be66a9cf810edda4b5590bd7a06
Matilda Sönnergaard <matilda.sonnergaard@se.experis.com>
2025-02-12 15:02:02 +0100

    Add map view to contacts

--- src/components/ViewContactPage.jsx
+++ src/components/ViewContactPage.jsx
@@ -1,6 +1,7 @@
 import { useContext } from 'react'
 import { AppContext } from '../App'
 import { useNavigate, useParams } from 'react-router-dom'
+import { ComposableMap, Geographies, Geography, Marker } from 'react-simple-maps'
 
 export default function ViewContactPage() {
   const context = useContext(AppContext)
@@ -36,6 +37,21 @@ export default function ViewContactPage() {
         onClick={() => navigate(`/contact/${id.id}/edit`)}>Edit</button>
       <button
         onClick={handleDelete}>Delete</button>
+      <ComposableMap
+        projection='geoEqualEarth'>
+        <Geographies
+        geography='/features.json'
+        >
+          {({geographies}) =>
+            geographies.map((geo) => (
+              <Geography key={geo.rmsKey} geography={geo} />
+            ))
+          }
+        </Geographies>
+        <Marker key='pos' coordinates={[contact.latitude, contact.longitude]}>
+          <circle r={10} fill='#f00' stroke='#fff' strokeWidth={2} />
+        </Marker>
+      </ComposableMap>
     </section>
   )
 }
