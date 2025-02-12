38941e9dde62b873471e658c2f217b5207a680fb
Matilda Sönnergaard <matilda.sonnergaard@se.experis.com>
2025-02-12 09:33:03 +0100

    Create new contact page (with only first name), send POST request

--- src/components/CreateContactPage.jsx
+++ src/components/CreateContactPage.jsx
@@ -1,5 +1,43 @@
+import { useState } from 'react'
+
 export default function CreateContactPage() {
+  const [formValues, setFormValues] = useState(
+    {firstName: '',
+      lastName: '',
+      street: '',
+      city: ''
+    }
+  )
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    const contact = {...formValues}
+    fetch('https://boolean-uk-api-server.fly.dev/pudkipz/contact', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(contact)
+    })
+  }
+
   return (
-    <h2>Create contact</h2>
+    <section className='create-contact'>
+      <form onSubmit={e => handleSubmit(e)}>
+        <h2>Create contact</h2>
+        <ul>
+          <li>
+            <b>First name:</b>
+          </li>
+          <li>
+            <input
+              onChange={e => setFormValues({...formValues, firstName: e.target.value})}
+              value={formValues.firstName}
+            />
+          </li>
+        </ul>
+        </form>
+    </section>
   )
 }
